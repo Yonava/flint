d7160b12aa330a864b15e97d0f8d6b8ba0cd66fd
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2026-04-30 20:35:33 -0400

    feat(package-json): [scriptsNameCasing] add rule (#2726)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #2582
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/flint-fyi/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/flint-fyi/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    ❤️‍🔥

--- packages/package-json/src/plugin.ts
+++ packages/package-json/src/plugin.ts
@@ -4,6 +4,7 @@ import { directPropertyPresenceRules } from "./directPropertyPresenceRules.ts";
 import { directPropertyValidityRules } from "./directPropertyValidityRules.ts";
 import binNameCasing from "./rules/binNameCasing.ts";
 import privatePresence from "./rules/privatePresence.ts";
+import scriptsNameCasing from "./rules/scriptsNameCasing.ts";
 
 export const packageJson = createPlugin({
 	files: {
@@ -13,6 +14,7 @@ export const packageJson = createPlugin({
 	rules: [
 		binNameCasing,
 		privatePresence,
+		scriptsNameCasing,
 		...Object.values(directPropertyPresenceRules),
 		...Object.values(directPropertyValidityRules),
 	],
