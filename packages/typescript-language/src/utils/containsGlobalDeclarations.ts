eff24e0b091d02f755e9b360a34a603bcd63f48e
yonava <yonavossandreae@gmail.com>
2026-06-04 23:39:29 +0200

    shortened js docs on containsGlobalDeclarations helper

--- packages/typescript-language/src/utils/containsGlobalDeclarations.ts
+++ packages/typescript-language/src/utils/containsGlobalDeclarations.ts
@@ -3,10 +3,6 @@ import ts from "typescript";
 /**
  * Inspects top-level statements of a TS source file to determine
  * if it introduces or modifies entities in the global scope.
- * @param rawFileContent The raw source code of the file to analyze.
- * @returns `true` if the file contains explicit global augmentations
- * (e.g., `declare global {}`) or top-level ambient declarations (e.g., `declare const`),
- * otherwise `false`.
  */
 export function containsGlobalDeclarations(rawFileContent: string) {
 	const sourceFileNode = ts.createSourceFile(
