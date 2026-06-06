d2e51a7c634fbf5b361ab12e55ff148bef4f7b06
michael faith <michaelfaith@users.noreply.github.com>
2026-06-06 15:03:16 -0500

    refactor(package-json): add new momoa-based remove fix helpers (#2904)
    <!-- 👋 Hi, thanks for sending a PR to flint! ❤️‍🔥
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [ ] Addresses an existing open issue: fixes #000
    - [ ] That issue was marked as [`status: accepting
    prs`](https://github.com/flint-fyi/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [ ] Steps in
    [CONTRIBUTING.md](https://github.com/flint-fyi/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    <!-- Description of what is changed and how the code change does that.
    -->
    
    ---------
    
    Co-authored-by: Josh Goldberg <git@joshuakgoldberg.com>

--- packages/package-json/src/removeArrayElement.ts
+++ packages/package-json/src/removeArrayElement.ts
@@ -1,41 +1,70 @@
 import type { ArrayNode, ElementNode } from "@humanwhocodes/momoa";
 
 import {
-	getJsonNodeRange as getLegacyJsonNodeRange,
+	getJsonNodeRange as getJsonNodeRangeLegacy,
 	type JsonSourceFile,
 } from "@flint.fyi/json-language";
-import { getJsonNodeRange as getNewJsonNodeRange } from "@flint.fyi/json-language/new";
+import { getJsonNodeRange } from "@flint.fyi/json-language/new";
 import type { AST } from "@flint.fyi/typescript-language";
 
 export function removeArrayElement(
-	sourceFile: JsonSourceFile,
-	element: AST.Expression,
-	arrayNode: AST.ArrayLiteralExpression,
-): ReturnType<typeof removeLegacyArrayElement>;
-export function removeArrayElement(
-	element: ElementNode,
+	elementNode: ElementNode,
 	arrayNode: ArrayNode,
-): ReturnType<typeof removeNewArrayElement>;
-export function removeArrayElement(
-	...parameters:
-		| [ElementNode, ArrayNode]
-		| [JsonSourceFile, AST.Expression, AST.ArrayLiteralExpression]
 ) {
-	if (parameters.length === 3) {
-		return removeLegacyArrayElement(...parameters);
+	if (arrayNode.elements.length === 1) {
+		return {
+			range: getJsonNodeRange(arrayNode),
+			text: "[]",
+		};
 	}
 
-	return removeNewArrayElement(...parameters);
+	const index = arrayNode.elements.indexOf(elementNode);
+	if (index === -1) {
+		throw new Error("Node is not a child of the parent array.");
+	}
+
+	const previous = index > 0 ? arrayNode.elements[index - 1] : undefined;
+	const next =
+		index < arrayNode.elements.length - 1
+			? arrayNode.elements[index + 1]
+			: undefined;
+
+	const { begin: elementBegin, end: elementEnd } = getJsonNodeRange(
+		elementNode.value,
+	);
+	if (next) {
+		const { begin: nextBegin } = getJsonNodeRange(next.value);
+		return {
+			range: {
+				begin: elementBegin,
+				end: nextBegin,
+			},
+			text: "",
+		};
+	}
+
+	if (previous) {
+		const { end: previousEnd } = getJsonNodeRange(previous.value);
+		return {
+			range: {
+				begin: previousEnd,
+				end: elementEnd,
+			},
+			text: "",
+		};
+	}
+
+	throw new Error("Expected array element to have a sibling.");
 }
 
-function removeLegacyArrayElement(
+export function removeArrayElementLegacy(
 	sourceFile: JsonSourceFile,
 	element: AST.Expression,
 	arrayNode: AST.ArrayLiteralExpression,
 ) {
 	if (arrayNode.elements.length === 1) {
 		return {
-			range: getLegacyJsonNodeRange(arrayNode, sourceFile),
+			range: getJsonNodeRangeLegacy(arrayNode, sourceFile),
 			text: "[]",
 		};
 	}
@@ -66,48 +95,6 @@ function removeLegacyArrayElement(
 			range: {
 				begin: previous.end,
 				end: element.end,
-			},
-			text: "",
-		};
-	}
-
-	throw new Error("Expected array element to have a sibling.");
-}
-
-function removeNewArrayElement(element: ElementNode, arrayNode: ArrayNode) {
-	if (arrayNode.elements.length === 1) {
-		return {
-			range: getNewJsonNodeRange(arrayNode),
-			text: "[]",
-		};
-	}
-
-	const index = arrayNode.elements.indexOf(element);
-	if (index === -1) {
-		throw new Error("Node is not a child of the parent array.");
-	}
-
-	const previous = index > 0 ? arrayNode.elements[index - 1] : undefined;
-	const next =
-		index < arrayNode.elements.length - 1
-			? arrayNode.elements[index + 1]
-			: undefined;
-
-	if (next) {
-		return {
-			range: {
-				begin: getNewJsonNodeRange(element.value).begin,
-				end: getNewJsonNodeRange(next.value).begin,
-			},
-			text: "",
-		};
-	}
-
-	if (previous) {
-		return {
-			range: {
-				begin: getNewJsonNodeRange(previous.value).end,
-				end: getNewJsonNodeRange(element.value).end,
 			},
 			text: "",
 		};
